d5868663144aa2edc3d872485906c40d98e75352
franconieves2 <114518842+franconieves2@users.noreply.github.com>
2022-09-29 13:40:14 -0500

    commented some stuff out
    ....

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -5,11 +5,11 @@ function seed() {
 function same([x, y], [j, k]) {
   return x === j && y == k; 
 }  
-function same([x, y], [j, k]) {}
+// function same([x, y], [j, k]) {}
 
 // The game state to search for `cell` is passed as the `this` value of the function.
 function contains(cell) { 
-  return this.some((c) => same(c, cell));
+  // return this.some((c) => same(c, cell));
 } 
 
 const printCell = (cell, state) => {
